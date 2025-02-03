67446fd5b0e3e1036f53ccc6e68612d387ae0852
Jaunty <jaunty5899@gmail.com>
2025-02-03 14:14:44 +0530

    app complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,30 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [convertedTemperature, setConvertedTemperature] = useState(0);
   const [temperature, setTemperature] = useState(0);
   const [from, setFrom] = useState();
   const [to, setTo] = useState();
+
+  function convertTemperature() {
+    setConvertedTemperature(() => {
+      if (from === "Celsius" && to === "Fahrenheit") {
+        return ((temperature * 9) / 5 + 32).toFixed(2);
+      } else if (from === "Celsius" && to === "Kelvin") {
+        return temperature + 273.15;
+      } else if (from === "Fahrenheit" && to === "Celsius") {
+        return (((temperature - 32) * 5) / 9).toFixed(2);
+      } else if (from === "Fahrenheit" && to === "Kelvin") {
+        return (((temperature - 32) * 5) / 9 + 273.15).toFixed(2);
+      } else if (from === "Kelvin" && to === "Celsius") {
+        return (temperature - 273.15).toFixed(2);
+      } else if (from === "Kelvin" && to === "Fahrenheit") {
+        return (((temperature - 273.15) * 9) / 5 + 32).toFixed(2);
+      } else {
+        return temperature;
+      }
+    });
+  }
 
   return (
     <div className="container">
@@ -32,12 +53,14 @@ function App() {
         </Dropdown>
         <button
           disabled={from && to ? false : true}
-          onClick={() => console.log("working")}
+          onClick={() => convertTemperature()}
         >
           Convert
         </button>
       </div>
-      <span className="result">Working</span>
+      <span className={`result ${convertedTemperature != 0 && "show"}`}>
+        {temperature}º{from} is {convertedTemperature}º{to}
+      </span>
     </div>
   );
 }
